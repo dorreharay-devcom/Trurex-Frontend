fc3d5b4d35067e0fc8346fc0537399ffd40da751
Oleksandr Smirnov <oleksandr.smirnov@Admins-MacBook-Pro.local>
2026-06-10 12:42:22 +0300

    change link

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -43,14 +43,11 @@ export const formatCurrency = (amount: number, currency: string = 'USD') => {
 
 const APP_SCHEME = process.env.EXPO_PUBLIC_APP_SCHEME ?? 'trurex';
 const AUTH_CALLBACK_PATH = 'auth/callback';
-const RESET_PASSWORD_PATH = 'reset-password';
+const RESET_PASSWORD_REDIRECT_URL = 'https://trurex.netlify.app/reset-password';
 
 export const getRedirectUrl = () =>
   isWeb
     ? `${window.location.origin}/${AUTH_CALLBACK_PATH}`
     : makeRedirectUri({ scheme: APP_SCHEME, path: AUTH_CALLBACK_PATH });
 
-export const getResetPasswordRedirectUrl = () =>
-  isWeb
-    ? `${window.location.origin}/${RESET_PASSWORD_PATH}`
-    : makeRedirectUri({ scheme: APP_SCHEME, path: RESET_PASSWORD_PATH });
+export const getResetPasswordRedirectUrl = () => RESET_PASSWORD_REDIRECT_URL;
