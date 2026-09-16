ead36c9e511724c3d294eae0946c208fe1123f15
Oleksandr Smirnov <oleksandr.smirnov@Admins-MacBook-Pro.local>
2026-09-16 17:00:36 +0300

    ui changes

--- src/shared/config/app.ts
+++ src/shared/config/app.ts
@@ -1,4 +1,5 @@
 export const TRUREX_WEB_ORIGIN = 'https://app.trurex.com';
+export const TRUREX_DEV_WEB_ORIGIN = 'https://trurex-dev.netlify.app';
 
 export const REX_IMAGES_BUCKET = 'rex-images';
 export const USER_AVATARS_BUCKET = 'user-avatars';
